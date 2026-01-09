3fcd6bf9a279a88208e6ce4a376ab0b8819f9747
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2026-01-09 14:44:41 +0300

    Common: update the makefiles logic

--- common/vivado.mk
+++ common/vivado.mk
@@ -40,18 +40,6 @@ CONFIG ?= config.mk
 FPGA_TOP ?= fpga
 PROJECT ?= $(FPGA_TOP)
 
-SYN_FILES_REL = $(foreach p,$(SYN_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-INC_FILES_REL = $(foreach p,$(INC_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-XCI_FILES_REL = $(foreach p,$(XCI_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-IP_TCL_FILES_REL = $(foreach p,$(IP_TCL_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-CONFIG_TCL_FILES_REL = $(foreach p,$(CONFIG_TCL_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-
-ifdef XDC_FILES
-  XDC_FILES_REL = $(foreach p,$(XDC_FILES),$(if $(filter /% ./%,$p),$p,../$p))
-else
-  XDC_FILES_REL = $(PROJECT).xdc
-endif
-
 # directory created by Vivado for some reason
 IP_GEN_DIR = ../../$(PROJECT).gen
 
@@ -70,60 +58,61 @@ vivado: $(PROJECT).xpr
 	vivado $(PROJECT).xpr
 
 tmpclean::
-	-rm -rf *.log *.jou *.cache *.gen *.hbs *.hw *.ip_user_files *.runs *.xpr *.html *.xml *.sim *.srcs *.str .Xil
-	-rm -rf create_project.tcl update_config.tcl run_synth.tcl run_impl.tcl generate_bit.tcl
-	-rm -rf $(IP_GEN_DIR)
+	@-rm -rf *.log *.jou *.cache *.gen *.hbs *.hw *.ip_user_files *.runs *.xpr *.html *.xml *.sim *.srcs *.str .Xil
+	@-rm -rf create_project.tcl update_config.tcl run_synth.tcl run_impl.tcl generate_bit.tcl
+	@-rm -rf $(IP_GEN_DIR)
 
 clean:: tmpclean
-	-rm -rf *.bit *.ltx program.tcl generate_mcs.tcl *.mcs *.prm flash.tcl
-	-rm -rf *_utilization.rpt *_utilization_hierarchical.rpt
+	@-rm -rf *.bit *.ltx program.tcl generate_mcs.tcl *.mcs *.prm flash.tcl
+	@-rm -rf *_utilization.rpt *_utilization_hierarchical.rpt
 
 ###################################################################
 # Target implementations
 ###################################################################
 
 # Vivado project file
-create_project.tcl: Makefile $(XCI_FILES_REL) $(IP_TCL_FILES_REL)
-	echo "create_project -force -part $(FPGA_PART) $(PROJECT)" > $@
-	echo "add_files -fileset sources_1 $(SYN_FILES_REL)" >> $@
-	echo "set_property top $(FPGA_TOP) [current_fileset]" >> $@
-	echo "add_files -fileset constrs_1 $(XDC_FILES_REL)" >> $@
-	for x in $(XCI_FILES_REL); do echo "import_ip $$x" >> $@; done
-	for x in $(IP_TCL_FILES_REL); do echo "source $$x" >> $@; done
-	for x in $(CONFIG_TCL_FILES_REL); do echo "source $$x" >> $@; done
+create_project.tcl: Makefile $(XCI_FILES) $(IP_TCL_FILES)
+	@echo "create_project -force -part $(FPGA_PART) $(PROJECT)" > $@
+	@if [ ! -z "$(SYN_FILES)" ]; then echo "add_files -fileset sources_1 $(SYN_FILES)" >> $@; fi
+	@if [ ! -z "$(SIM_FILES)" ]; then echo "add_files -fileset sim_1 $(SIM_FILES)" >> $@; fi
+	@echo "set_property top $(FPGA_TOP) [current_fileset]" >> $@
+	@if [ ! -z "$(XDC_FILES)" ]; then echo "add_files -fileset constrs_1 $(XDC_FILES)" >> $@; fi
+	@for x in $(XCI_FILES); do echo "import_ip $$x" >> $@; done
+	@for x in $(IP_TCL_FILES); do echo "source $$x" >> $@; done
+	@for x in $(CONFIG_TCL_FILES); do echo "source $$x" >> $@; done
 
-update_config.tcl: $(CONFIG_TCL_FILES_REL) $(SYN_FILES_REL) $(INC_FILES_REL) $(XDC_FILES_REL)
-	echo "open_project -quiet $(PROJECT).xpr" > $@
-	for x in $(CONFIG_TCL_FILES_REL); do echo "source $$x" >> $@; done
+update_config.tcl: $(CONFIG_TCL_FILES) $(SYN_FILES) $(INC_FILES) $(XDC_FILES)
+	@echo "open_project -quiet $(PROJECT).xpr" > $@
+	@for x in $(CONFIG_TCL_FILES); do echo "source $$x" >> $@; done
 
 $(PROJECT).xpr: create_project.tcl update_config.tcl
 	vivado -nojournal -nolog -mode batch $(foreach x,$?,-source $x)
 
 # synthesis run
-$(PROJECT).runs/synth_1/$(PROJECT).dcp: create_project.tcl update_config.tcl $(SYN_FILES_REL) $(INC_FILES_REL) $(XDC_FILES_REL) | $(PROJECT).xpr
-	echo "open_project $(PROJECT).xpr" > run_synth.tcl
-	echo "reset_run synth_1" >> run_synth.tcl
-	echo "launch_runs -jobs 4 synth_1" >> run_synth.tcl
-	echo "wait_on_run synth_1" >> run_synth.tcl
+$(PROJECT).runs/synth_1/$(PROJECT).dcp: create_project.tcl update_config.tcl $(SYN_FILES) $(INC_FILES) $(XDC_FILES) | $(PROJECT).xpr
+	@echo "open_project $(PROJECT).xpr" > run_synth.tcl
+	@echo "reset_run synth_1" >> run_synth.tcl
+	@echo "launch_runs -jobs 4 synth_1" >> run_synth.tcl
+	@echo "wait_on_run synth_1" >> run_synth.tcl
 	vivado -nojournal -nolog -mode batch -source run_synth.tcl
-	if [ -e $(IP_GEN_DIR) ]; then rm -rf $(IP_GEN_DIR); fi
+	@if [ -e $(IP_GEN_DIR) ]; then rm -rf $(IP_GEN_DIR); fi
 
 # implementation run
 $(PROJECT).runs/impl_1/$(PROJECT)_routed.dcp: $(PROJECT).runs/synth_1/$(PROJECT).dcp
-	echo "open_project $(PROJECT).xpr" > run_impl.tcl
-	echo "reset_run impl_1" >> run_impl.tcl
-	echo "launch_runs -jobs 4 impl_1" >> run_impl.tcl
-	echo "wait_on_run impl_1" >> run_impl.tcl
-	echo "open_run impl_1" >> run_impl.tcl
-	echo "report_utilization -file $(PROJECT)_utilization.rpt" >> run_impl.tcl
-	echo "report_utilization -hierarchical -file $(PROJECT)_utilization_hierarchical.rpt" >> run_impl.tcl
+	@echo "open_project $(PROJECT).xpr" > run_impl.tcl
+	@echo "reset_run impl_1" >> run_impl.tcl
+	@echo "launch_runs -jobs 4 impl_1" >> run_impl.tcl
+	@echo "wait_on_run impl_1" >> run_impl.tcl
+	@echo "open_run impl_1" >> run_impl.tcl
+	@echo "report_utilization -file $(PROJECT)_utilization.rpt" >> run_impl.tcl
+	@echo "report_utilization -hierarchical -file $(PROJECT)_utilization_hierarchical.rpt" >> run_impl.tcl
 	vivado -nojournal -nolog -mode batch -source run_impl.tcl
-	if [ -e $(IP_GEN_DIR) ]; then rm -rf $(IP_GEN_DIR); fi
+	@if [ -e $(IP_GEN_DIR) ]; then rm -rf $(IP_GEN_DIR); fi
 
 # bit file
 $(PROJECT).bit $(PROJECT).ltx: $(PROJECT).runs/impl_1/$(PROJECT)_routed.dcp
-	echo "open_project $(PROJECT).xpr" > generate_bit.tcl
-	echo "open_run impl_1" >> generate_bit.tcl
-	echo "write_bitstream -force $(PROJECT).runs/impl_1/$(PROJECT).bit" >> generate_bit.tcl
-	echo "write_debug_probes -force $(PROJECT).runs/impl_1/$(PROJECT).ltx" >> generate_bit.tcl
+	@echo "open_project $(PROJECT).xpr" > generate_bit.tcl
+	@echo "open_run impl_1" >> generate_bit.tcl
+	@echo "write_bitstream -force $(PROJECT).runs/impl_1/$(PROJECT).bit" >> generate_bit.tcl
+	@echo "write_debug_probes -force $(PROJECT).runs/impl_1/$(PROJECT).ltx" >> generate_bit.tcl
 	vivado -nojournal -nolog -mode batch -source generate_bit.tcl
