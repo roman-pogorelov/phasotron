aa7637b03aa944e94cf4737278ab9f7ccc3795ff
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2026-09-01 14:21:06 +0300

    Common: APB3: update APB3 interface definition

--- common/src/rtl/apb3/apb3.sv
+++ common/src/rtl/apb3/apb3.sv
@@ -1,14 +1,33 @@
+// APB3 definitions package
+package apb3_defs;
+
+    // APB3 address
+    typedef struct packed {
+        logic [29 : 0]  word_addr;
+        logic [1 : 0]   byte_idx;
+    } apb3_addr_t;
+
+
+    // APB3 data
+    typedef logic [31 : 0] apb3_data_t;
+
+endpackage: apb3_defs
+
+
+// APB3 interface
 interface apb3_if;
 
+    import apb3_defs::*;
+
     // Interface signals
-    logic [31 : 0]  paddr;
-    logic           psel;
-    logic           penable;
-    logic           pwrite;
-    logic [31 : 0]  pwdata;
-    logic           pready;
-    logic [31 : 0]  prdata;
-    logic           pslverr;
+    apb3_addr_t paddr;
+    logic       psel;
+    logic       penable;
+    logic       pwrite;
+    apb3_data_t pwdata;
+    logic       pready;
+    apb3_data_t prdata;
+    logic       pslverr;
 
 
     // Master mode
