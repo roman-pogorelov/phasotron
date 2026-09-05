1727a54a22fa9781f37eb8e07b7d5064ee730f48
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2026-09-05 17:49:08 +0300

    TIP: implement the TIP upstream endpoint router

--- common/src/rtl/tip/tip_up_router.sv
+++ common/src/rtl/tip/tip_up_router.sv
@@ -27,6 +27,8 @@
     ); // the_tip_up_router
 */
 
+import tip_defs::*;
+
 module tip_up_router
 (
     // Reset and clock
@@ -53,36 +55,176 @@ module tip_up_router
     axis_if.master          dn_ctrl_out,
     axis_if.slave           dn_data_in
 );
-    // FIXME:
-    assign up_transp_out.tdata = up_transp_out.tdata;
-    assign up_transp_out.tkeep = up_transp_out.tkeep;
-    assign up_transp_out.tvalid = up_transp_out.tvalid;
-    assign up_transp_out.tlast = up_transp_out.tlast;
-    assign up_transp_in.tready = up_transp_in.tready;
+    // Interfaces
+    axis_if         to_up_transp[4]();
+    axis_if         to_up_transp_buf();
+    //
+    axis_if         from_up_transp[3]();
+    //
+    tip_pkt_hdr_t   transp_in_hdr;
+    logic           is_tkeep_valid;
+    logic           does_addr_match;
+    //
+    logic           is_conf_cmd;
+    logic           is_user_cmd;
+    logic           is_ctrl_pkt;
 
 
-    // FIXME:
-    assign conf_cmd_out.tdata = '0;
-    assign conf_cmd_out.tkeep = '0;
-    assign conf_cmd_out.tvalid = '0;
-    assign conf_cmd_out.tlast = '0;
-    assign conf_rpt_in.tready = '1;
+    // Connects AXIS interfaces
+    axis_if_connect conf_rpt_in_connect (
+        // Slave interface
+        .s      (conf_rpt_in),      // axis_if.slave
+
+        // Master interface
+        .m      (to_up_transp[0])   // axis_if.master
+    ); // conf_rpt_in_connect
 
 
-    // FIXME:
-    assign user_cmd_out.tdata = '0;
-    assign user_cmd_out.tkeep = '0;
-    assign user_cmd_out.tvalid = '0;
-    assign user_cmd_out.tlast = '0;
-    assign user_rpt_in.tready = '1;
+    // Connects AXIS interfaces
+    axis_if_connect user_rpt_in_connect (
+        // Slave interface
+        .s      (user_rpt_in),      // axis_if.slave
+
+        // Master interface
+        .m      (to_up_transp[1])   // axis_if.master
+    ); // user_rpt_in_connect
 
 
-    // FIXME:
-    assign dn_ctrl_out.tdata = '0;
-    assign dn_ctrl_out.tkeep = '0;
-    assign dn_ctrl_out.tvalid = '0;
-    assign dn_ctrl_out.tlast = '0;
-    assign dn_ctrl_in.tready = '1;
-    assign dn_data_in.tready = '1;
+    // Connects AXIS interfaces
+    axis_if_connect dn_ctrl_in_connect (
+        // Slave interface
+        .s      (dn_ctrl_in),      // axis_if.slave
+
+        // Master interface
+        .m      (to_up_transp[2])   // axis_if.master
+    ); // dn_ctrl_in_connect
+
+
+    // Connects AXIS interfaces
+    axis_if_connect dn_data_in_connect (
+        // Slave interface
+        .s      (dn_data_in),      // axis_if.slave
+
+        // Master interface
+        .m      (to_up_transp[3])   // axis_if.master
+    ); // dn_data_in_connect
+
+
+    // AXIS arbiter
+    axis_arbiter #(
+        .IN_CNT     (4),                // Number of input streams
+        .SCHEME     ("FP")              // Arbitration scheme ("RR" - round-robin, "FP" - fixed priorities)
+    )
+    up_transp_out_arbiter (
+        // Reset and clock
+        .rst        (rst),              // i
+        .clk        (clk),              // i
+
+        // Input AXIS interface
+        .in         (to_up_transp),     // axis_if[IN_CNT].slave
+
+        // Output AXIS interface
+        .out        (to_up_transp_buf)  // axis_if.master
+    ); // up_transp_out_arbiter
+
+
+    // 2-stage AXIS buffer
+    axis_2stage_buf up_transp_out_buf (
+        // Reset and clock
+        .rst        (rst),              // i
+        .clk        (clk),              // i
+
+        // Input AXIS interface
+        .in         (to_up_transp_buf), // axis_if.slave
+
+        // Output AXIS interface
+        .out        (up_transp_out)     // axis_if.master
+    ); // up_transp_out_buf
+
+
+    // Forwards the input stream to a mask-selected
+    // subset of multiple outputs
+    axis_multicast #(
+        .OUT_CNT    (3)                 // Number of output stream
+    )
+    up_transp_in_multicast (
+        // Reset and clock
+        .rst        (rst),              // i
+        .clk        (clk),              // i
+
+        // Mask selecting the active outputs
+        .out_mask   ({
+                        is_ctrl_pkt,
+                        is_user_cmd,
+                        is_conf_cmd
+                    }),                 // i  [OUT_CNT - 1 : 0]
+
+        // Input AXIS interface
+        .in         (up_transp_in),     // axis_if.slave
+
+        // Output AXIS interface
+        .out        (from_up_transp)    // axis_if[OUT_CNT].master
+    ); // up_transp_in_multicast
+
+
+    // 2-stage AXIS buffer
+    axis_2stage_buf conf_cmd_out_buf (
+        // Reset and clock
+        .rst        (rst),                  // i
+        .clk        (clk),                  // i
+
+        // Input AXIS interface
+        .in         (from_up_transp[0]),    // axis_if.slave
+
+        // Output AXIS interface
+        .out        (conf_cmd_out)          // axis_if.master
+    ); // conf_cmd_out_buf
+
+
+    // 2-stage AXIS buffer
+    axis_2stage_buf user_cmd_out_buf (
+        // Reset and clock
+        .rst        (rst),                  // i
+        .clk        (clk),                  // i
+
+        // Input AXIS interface
+        .in         (from_up_transp[1]),    // axis_if.slave
+
+        // Output AXIS interface
+        .out        (user_cmd_out)          // axis_if.master
+    ); // user_cmd_out_buf
+
+
+    // 2-stage AXIS buffer
+    axis_2stage_buf dn_ctrl_out_buf (
+        // Reset and clock
+        .rst        (rst),                  // i
+        .clk        (clk),                  // i
+
+        // Input AXIS interface
+        .in         (from_up_transp[2]),    // axis_if.slave
+
+        // Output AXIS interface
+        .out        (dn_ctrl_out)           // axis_if.master
+    ); // dn_ctrl_out_buf
+
+
+    // Identify the traffic coming from the transport
+    assign transp_in_hdr = up_transp_in.tdata;
+    assign is_tkeep_valid = &up_transp_in.tkeep[TIP_AXIS_TKEEP_W - 1 : 0];
+    assign does_addr_match = (
+        (transp_in_hdr.cmn.node_addr == up_conf.indiv_addr) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr0) & (up_conf.group_addr0 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr1) & (up_conf.group_addr1 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr2) & (up_conf.group_addr2 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr3) & (up_conf.group_addr3 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr4) & (up_conf.group_addr4 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr5) & (up_conf.group_addr5 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr6) & (up_conf.group_addr6 != 0)) |
+        ((transp_in_hdr.cmn.node_addr == up_conf.group_addr7) & (up_conf.group_addr7 != 0))
+    );
+    assign is_conf_cmd = is_tkeep_valid & (transp_in_hdr.cmn.type_id == TIP_PKT_TYPE_CMD_CFG) & does_addr_match;
+    assign is_user_cmd = is_tkeep_valid & (transp_in_hdr.cmn.type_id == TIP_PKT_TYPE_CMD_USR) & does_addr_match;;
+    assign is_ctrl_pkt = is_tkeep_valid & ((transp_in_hdr.cmn.type_id == TIP_PKT_TYPE_CMD_CFG) | (transp_in_hdr.cmn.type_id == TIP_PKT_TYPE_CMD_USR));
 
 endmodule: tip_up_router
